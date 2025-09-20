3230a062f53c667d01930d755eda651b90a554eb
shrysshety97 <122443770+shrysshety97@users.noreply.github.com>
2025-09-20 15:16:04 +0530

    Update QuizContext.js

--- src/contexts/QuizContext.js
+++ src/contexts/QuizContext.js
@@ -83,7 +83,7 @@ function QuizProvider({ children }) {
   );
 
   useEffect(function () {
-    fetch("http://localhost:9000/questions")
+    fetch("/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
